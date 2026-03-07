c1a7ac970920e7271c9fcfba76f8c0d313abe785
Thisal De Costa <thisalthemiya525@gmail.com>
2026-03-07 18:37:37 +0530

    1

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("CI/CD Pipeline Working!");
 });
 
-app.listen(5001, () => {
-  console.log("Server ru2nning on port 5000000000");
+app.listen(5000, () => {
+  console.log("Se0000rver ru2nning on port 5000000000");
 });
